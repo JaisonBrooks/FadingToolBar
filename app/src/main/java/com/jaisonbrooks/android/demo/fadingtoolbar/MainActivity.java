d4540f8fc2da08dbdc32a5791799120f82836293
Jaison Brooks <jbrooks@enphaseenergy.com>
2014-10-30 11:41:43 -0700

    initial commit

---
+++ app/src/main/java/com/jaisonbrooks/android/demo/fadingtoolbar/MainActivity.java
@@ -0,0 +1,134 @@
+package com.jaisonbrooks.android.demo.fadingtoolbar;
+
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.manuelpeinado.fadingactionbar.view.ObservableScrollable;
+import com.manuelpeinado.fadingactionbar.view.OnScrollChangedCallback;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+
+public class MainActivity extends ActionBarActivity implements OnScrollChangedCallback {
+
+    private Toolbar mToolbar;
+    private Drawable mActionBarBackgroundDrawable;
+    private View mHeader;
+    private int mLastDampedScroll;
+    private int mInitialStatusBarColor;
+    private int mFinalStatusBarColor;
+    private SystemBarTintManager mStatusBarManager;
+    private int lastScrollPosition;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mActionBarBackgroundDrawable = mToolbar.getBackground();
+        setSupportActionBar(mToolbar);
+
+        mStatusBarManager = new SystemBarTintManager(this);
+        mStatusBarManager.setStatusBarTintEnabled(true);
+
+        mInitialStatusBarColor = getResources().getColor(R.color.background_material_dark);
+        mFinalStatusBarColor = getResources().getColor(R.color.colorPrimaryDark);
+
+        mHeader = findViewById(R.id.header);
+
+        ObservableScrollable scrollView = (ObservableScrollable) findViewById(R.id.scrollview);
+        scrollView.setOnScrollChangedCallback(this);
+
+        onScroll(-1, 0);
+    }
+
+
+    public void onScroll(int l, int scrollPosition) {
+        int headerHeight = mHeader.getHeight() - mToolbar.getHeight();
+        float ratio = 0;
+        if (scrollPosition > 0 && headerHeight > 0)
+            ratio = (float) Math.min(Math.max(scrollPosition, 0), headerHeight) / headerHeight;
+
+        updateActionBarTransparency(ratio);
+        updateStatusBarColor(ratio);
+        updateParallaxEffect(scrollPosition);
+        lastScrollPosition = scrollPosition;
+    }
+
+    private void updateActionBarTransparency(float scrollRatio) {
+        int newAlpha = (int) (scrollRatio * 255);
+        mActionBarBackgroundDrawable.setAlpha(newAlpha);
+        if (Build.VERSION.SDK_INT >= 16) {
+            mToolbar.setBackground(mActionBarBackgroundDrawable);
+        } else {
+            mToolbar.setBackgroundDrawable(mActionBarBackgroundDrawable);
+        }
+
+    }
+
+    private void updateStatusBarColor(float scrollRatio) {
+        int r = interpolate(Color.red(mInitialStatusBarColor), Color.red(mFinalStatusBarColor), 1 - scrollRatio);
+        int g = interpolate(Color.green(mInitialStatusBarColor), Color.green(mFinalStatusBarColor), 1 - scrollRatio);
+        int b = interpolate(Color.blue(mInitialStatusBarColor), Color.blue(mFinalStatusBarColor), 1 - scrollRatio);
+        mStatusBarManager.setTintColor(Color.rgb(r, g, b));
+    }
+
+    private void updateParallaxEffect(int scrollPosition) {
+        float damping = 0.5f;
+        int dampedScroll = (int) (scrollPosition * damping);
+        int offset = mLastDampedScroll - dampedScroll;
+        mHeader.offsetTopAndBottom(-offset);
+
+        mLastDampedScroll = dampedScroll;
+    }
+
+    private int interpolate(int from, int to, float param) {
+        return (int) (from * param + to * (1 - param));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Toast.makeText(MainActivity.this, "No Settings", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+/*    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            //Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+           // onScroll(0, lastScrollPosition);
+            onScroll(-1, 0);
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            //Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+           // onScroll(0, lastScrollPosition);
+            onScroll(-1, 0);
+        }
+    }*/
+}
